74fc0c892b188cd346bc5d3b362a48b6124b2a95
khanhlinhoabinh <khanhlinhphamhb@gmail.com>
2026-06-05 11:40:31 +0700

    1

--- src/components/layout/Sidebar.jsx
+++ src/components/layout/Sidebar.jsx
@@ -11,13 +11,13 @@ import {
   Settings,
   Plus,
 } from "lucide-react";
-import { useNavigate } from "react-router-dom";
+import { useNavigate, useLocation } from "react-router-dom";
 export default function Sidebar() {
-    const navigate = useNavigate();
-
+  const navigate = useNavigate();
+  const location = useLocation();
   const navItems = [
     { icon: Home, label: "Trang chủ", path: "/", },
-    { icon: BookOpen, label: "Môn học của tôi",path: "/mysubjects", },
+    { icon: BookOpen, label: "Môn học của tôi", path: "/mysubjects", },
     { icon: FileText, label: "Tài liệu" },
     { icon: MessageCircle, label: "Chat AI" },
     { icon: StickyNote, label: "Ghi chú" },
@@ -42,9 +42,8 @@ export default function Sidebar() {
           return (
             <div
               key={index}
-              className={`nav-item ${
-                index === 0 ? "active" : ""
-              }`}
+              className={`nav-item ${location.pathname === item.path ? "active" : ""
+                }`}
               onClick={() => {
                 if (item.path) {
                   navigate(item.path);
